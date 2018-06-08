75096aeb4b7b08912a9ed136a78006c06505b11c
neirac <cneirabustos@gmail.com>
2018-06-07 20:52:53 -0400

    zcage start

--- node-illumos-zones/main.js
+++ node-illumos-zones/main.js
@@ -1,3 +1,9 @@
+/* zcage tries to perform the same tasks as iocage but for illumos
+ * http://iocage.readthedocs.io/en/latest/basic-use.html#activate-iocage
+ * command syntax will be like iocage
+ */
+
+
 var fs = require('fs'),
     xml2js = require('xml2js');
 const
@@ -88,3 +94,52 @@ function ExecZonecmd(zonename, cmd) {
    console.log("info ", zlogin.stdout.toString());
    return zlogin.stdout.toString();		
 }
+
+function StartZone(zonename) {
+   var start = spawnSync( 'pfexec', ['zoneadm', '-z', zonename, 'boot' ] );
+   console.log("info ", start.stdout.toString());
+   return start.stdout.toString();		
+}
+
+function StopZone(zonename) {
+   var stop = spawnSync( 'pfexec', ['zoneadm', '-z', zonename, 'halt' ] );
+   console.log("info ", stop.stdout.toString());
+   return stop.stdout.toString();		
+}
+
+function RebootZone(zonename) {
+   var reboot = spawnSync( 'pfexec', ['zoneadm', '-z', zonename, 'reboot' ] );
+   console.log("info ", reboot.stdout.toString());
+   return reboot.stdout.toString();		
+}
+
+function SetZoneProperty(zonename,property) {
+   var zonedata = GetZoneData(zonename);
+   var key = property.keys();
+   var val = property.values();
+   zonedata[key] = val;
+   return zonedata;
+}
+function GetZoneProperty(zonename,property) {
+   var zonedata = GetZoneData(zonename);
+   var key = property.keys();
+   return zonedata[key] 
+
+}
+
+
+//TODO: 
+// define props object that will contains config data to create zone
+// props name should represent a config section.
+function CreateZone(props) {
+//   var zonecfg = spawnSync( 'pfexec', ['zonecfg', 'create -b', zoneobj.zonename ] );
+   var minimal = {zonename: "", zonepath: "", brand: "lx", 'ip-type': "exclusive" ,
+		net: [ { physical: "", gateway: "", ips: [""], primary: true } ],
+		'dns-domain': "", resolvers: [""],'kernel-version': ""};
+   let merge = { minimal,props};
+  console.log("props", JSON.stringify(merge, null, 4)); 
+}
+var o = { test: "1"};
+CreateZone(o);
+
+
